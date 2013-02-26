69f995138bf43ef5acce0b1e457fde36195279ed
abmti <byadriano@gmail.com>
2013-02-26 14:38:51 -0300

    Commit

--- web/index.php
+++ web/index.php
@@ -3,45 +3,26 @@
 ini_set("display_errors", true );
 ini_set("html_errors", true);
 
-// Include ClassLoaders
-require_once("../application/phpx/util/ClassLoader.php");
-require_once("../library/Doctrine/Common/ClassLoader.php");
-
-// Autoloader (App)
-$appClassLoader = new \phpx\util\ClassLoader("../application");
-$appClassLoader->register();
+require_once("../vendor/autoload.php");
 
 // Autoloader (Build)
 $appClassLoader = new \phpx\util\ClassLoader("../build");
 $appClassLoader->register();
 
-// Autoloader (Doctrine)
-$classLoader = new \Doctrine\Common\ClassLoader("Doctrine", "../library");
-$classLoader->register();
-
-// Autoloader (Library)
-$facesClassLoader = new \phpx\util\ClassLoader("../library");
-$facesClassLoader->register();
-
-// Include Log4php
-require_once("../library/log4php/Logger.php");
-Logger::configure("../application/appender_file.properties");
-
-// Include CuteEditor
-require_once("../library/cuteeditor/CuteEditor.php");
+Logger::configure("../src/appender_file.properties");
 
 $path = \phpx\util\Path::getInstance();
 $path->addPath("PATH", realpath("../"));
 $path->addPath("PATH_PUBLIC", realpath("."));
-$path->addPath("PATH_APPLICATION", realpath("../")."/application");
-$path->addPath("PATH_PHPX", realpath("../")."/application");
-$path->addPath("CONTEXT_RESOURCES", "../application/phpx/resources");
+$path->addPath("PATH_APPLICATION", realpath("../")."/src");
+$path->addPath("PATH_PHPX", realpath("../")."/src");
+$path->addPath("CONTEXT_RESOURCES", "../src/phpx/resources");
 
-\Doctrine\Common\Annotations\AnnotationRegistry::registerAutoloadNamespace("Ray", dirname(__DIR__) . '/library');
+\Doctrine\Common\Annotations\AnnotationRegistry::registerAutoloadNamespace("Ray", dirname(__DIR__) . '/vendor/others');
 \Doctrine\Common\Annotations\AnnotationRegistry::registerAutoloadNamespace("phpx\\seam\\util\\annotation", \phpx\util\Path::getInstance()->getPath("PATH_PHPX"));
 
 $facesServlet = new \phpx\faces\FacesServlet();
-$facesServlet->init("phpseam");
+$facesServlet->init("phpx");
 $facesServlet->service();
 
 ?>
